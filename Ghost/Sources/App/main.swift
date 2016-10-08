6103035b9755715b67cca4fa422db1636b43e745
Robert Mißbach <mail@robert-missbach.com>
2016-10-09 00:04:38 +0200

    Clean up Swift code

--- Ghost/Sources/App/main.swift
+++ Ghost/Sources/App/main.swift
@@ -3,10 +3,13 @@ import Vapor
 let drop = Droplet()
 
 // For the root we return an HTML page
-drop.get { req in
-    let lang = req.headers["Accept-Language"]?.string ?? "en"
+drop.get { request in
+    
+    let defaultLanguage = "en"
+    let language = request.headers["Accept-Language"]?.string ?? defaultLanguage
+    
     return try drop.view.make("index", [
-    	"message": Node.string(drop.localization[lang, "index", "title"])
+    	"message": Node.string(drop.localization[language, "index", "title"])
     ])
 }
 
