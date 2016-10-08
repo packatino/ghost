35786480a9dc496bce28585d1bdb8be3942f1cf7
Robert Mißbach <mail@robert-missbach.com>
2016-10-08 23:24:25 +0200

    Create target for unit tests

--- Ghost/Package.swift
+++ Ghost/Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         "Localization",
         "Public",
         "Resources",
-        "Tests",
     ]
 )
 
---
+++ Ghost/Tests/UnitTests/ExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleTests.swift
+//  Ghost
+//
+//  Created by Robert Mißbach on 08.10.16.
+//
+//
+
+import XCTest
+
+class ExampleTests: XCTestCase {
+    
+    func testExample()
+    {
+        XCTAssertTrue(true)
+    }
+}
