4dd18b35d30be74623e0eca90ecf357340b9394b
Robert Mißbach <mail@robert-missbach.com>
2016-10-19 21:20:01 +0200

    Make docker build script easier to understand

--- Ghost/build_docker_image.sh
+++ Ghost/build_docker_image.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Get the current commit hash (short version)
 TAG=$(git rev-parse --short HEAD); readonly TAG
+
+# Build the image
 docker build -t packatino/ghost:${TAG} --build-arg SWIFT_VERSION=$(cat .swift-version) .
